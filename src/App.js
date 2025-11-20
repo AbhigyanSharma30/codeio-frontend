813ad0c43fcb5e401fcf4a52a42c19c967a3a174
AbhigyanSharma30 <2300030805@kluniversity.in>
2025-11-20 23:24:36 +0530

    fix final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router , Routes , Route } from 'react-router-dom';
 import CollaborativeEditor from './components/CollaborativeEditor';
 import Auth from './components/Auth';
 import Home from './components/Home';
